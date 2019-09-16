2a30ad8684d12e3a8483bd078a04260fdffd34f4
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-16 20:49:04 +0300

    Clear code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { version, Component } from 'inferno';
-import Logo from './logo';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Logo width="80" height="80" />
-          <p>{`Welcome to Inferno ${version}`}</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app"></div>
+);
 
 export default App;
