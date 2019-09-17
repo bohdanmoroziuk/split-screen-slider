afaad8b6748a342114f940ad47ee5fd6fb7a3374
Bogdan <bogdan7morozyuk@gmail.com>
2019-09-17 21:10:41 +0300

    Add dummy data

---
+++ src/static/data/layers.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    position: 'bottom',
+    image: {
+      src: "/images/layers/layer-1.png",
+      alt: ""
+    },
+    caption: "Alias, consequatur ipsa",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  }, {
+    position: 'top',
+    image: {
+      src: "/images/layers/layer-2.png",
+      alt: ""
+    },
+    caption: "Lorem ipsum dolor",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+];
